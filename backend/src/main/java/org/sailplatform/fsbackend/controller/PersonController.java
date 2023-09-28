0cd291788ad83a6d31c2b40df920ff0d0aef07a2
unknown <zzhzhangzhihan@yahoo.com>
2023-09-28 17:52:56 -0400

    Front End and Back End tested, update & delete backend added

--- backend/src/main/java/org/sailplatform/fsbackend/controller/PersonController.java
+++ backend/src/main/java/org/sailplatform/fsbackend/controller/PersonController.java
@@ -6,8 +6,10 @@ import org.sailplatform.fsbackend.model.Person;
 import org.sailplatform.fsbackend.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,5 +26,15 @@ public class PersonController {
     @PostMapping("/add")
 	public Person add(Person toAdd) {
 		return personService.add(toAdd);
+	}
+
+    @PutMapping("/update/{id}")
+	public Person update(Person toUpdate, Long id) {
+		return personService.update(toUpdate, id);
+	}
+    
+    @DeleteMapping("/delete/{id}")
+	public void delete(Long id) {
+		personService.delete(id);
 	}
 }
