402e67bbd89653353128a066b8a083ee7bd632b4
zhihanzzh <zzhzhangzhihan@yahoo.com>
2024-12-02 17:04:36 -0500

    dummy change on test branch 3

--- backend/src/main/java/org/sailplatform/fsbackend/controller/PersonController.java
+++ backend/src/main/java/org/sailplatform/fsbackend/controller/PersonController.java
@@ -47,6 +47,7 @@ public class PersonController {
 		System.out.println("test");
 		System.out.println("test");
 		System.out.println("test");
+		System.out.println("test");
 		return personService.searchByFirstName(firstName);
 	}
 
